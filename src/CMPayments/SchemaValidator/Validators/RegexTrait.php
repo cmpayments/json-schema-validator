5c532f23546b6c081e8806c71154233952fe6382
Rob Theeuwes <rob.theeuwes@cm.nl>
2016-02-16 13:23:28 +0100

    changed validation and comparisons

--- src/CMPayments/SchemaValidator/Validators/RegexTrait.php
+++ src/CMPayments/SchemaValidator/Validators/RegexTrait.php
@@ -1,45 +1,44 @@
-<?php
-
-namespace CMPayments\SchemaValidator\Validators;
+<?php namespace CMPayments\SchemaValidator\Validators;
 
 use CMPayments\SchemaValidator\BaseValidator;
 use CMPayments\SchemaValidator\Exceptions\ValidateException;
 
-trait RegexTrait {
+trait RegexTrait
+{
 
-  /**
-   * Validates $data against a specific $schema->regex
-   *
-   * @param $data
-   * @param $schema
-   * @param $path
-   */
-  public function validateRegex($data, $schema, $path) {
-	if (!isset($schema->pattern)) {
-	  return;
-	}
-	
-	$pattern = '/'.trim($schema->pattern, '/').'/';
+    /**
+     * Validates $data against a specific $schema->regex
+     *
+     * @param $data
+     * @param $schema
+     * @param $path
+     */
+    public function validateRegex($data, $schema, $path)
+    {
+        if (!isset($schema->pattern)) {
+            return;
+        }
 
-	if ( ($schema->type != 'number' || $schema->type != 'string') && !is_scalar($data)) {
-	  $this->addError(ValidateException::ERROR_USER_REGEX_DATA_NOT_SCALAR, [$data]);
-	}
+        $pattern = '/' . trim($schema->pattern, '/') . '/';
 
-	/**
-	 * Use try catch to be able to handle malformed regex
-	 */
-	try {
-	  
-	  if (!preg_match($pattern, $data)) {
-		$this->addError(ValidateException::ERROR_USER_REGEX_NOMATCH, [$data, $schema->pattern]);
-	  }
-	  
-	} catch (Exception $ex) {
-		$this->addError(ValidateException::ERROR_USER_REGEX_PATTERN_NOT_VALID, [$schema->pattern, $data]);
-	  
-	}
-  }
+        if (($schema->type !== BaseValidator::NUMBER || $schema->type !== BaseValidator::STRING) && !is_scalar($data)) {
+            $this->addError(ValidateException::ERROR_USER_REGEX_DATA_NOT_SCALAR, [$data]);
+        }
 
+        /**
+         * Use try catch to be able to handle malformed regex
+         */
+        try {
+            $result = preg_match($pattern, $data);
+            if ($result === false) {
+                $this->addError(ValidateException::ERROR_USER_REGEX_NOMATCH, [$data, $schema->pattern]);
+            } elseif ($result === null) {
+                $this->addError(ValidateException::ERROR_USER_REGEX_PATTERN_NOT_VALID, [$schema->pattern, $data]);
+            }
+        } catch (\Exception $ex) {
+            $this->addError(ValidateException::ERROR_USER_REGEX_PATTERN_NOT_VALID, [$schema->pattern, $data]);
+        }
+    }
 
 
 }
