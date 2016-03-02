685a3eeb4cb90562bc3e0387e338852645bbe4ac
Boy WIjnmaalen <boy.wijnmaalen@home.nl>
2016-03-02 12:31:16 +0100

    added method convert_exception_to_array() in duplicate, one for < PHP 7 and one for >= PHP7, fixed where if the required property for a schema was set but was left empty it would result in an error

--- src/CMPayments/helpers.php
+++ src/CMPayments/helpers.php
@@ -1,27 +1,56 @@
 <?php
 
 if (!function_exists('convert_exception_to_array')) {
-    /**
-     * Converts an \Exception into an array
-     *
-     * @param Exception $e
-     *
-     * @return string
-     */
-    function convert_exception_to_array(\Exception $e)
-    {
-        $destination        = new \stdClass();
-        $destination->class = get_class($e);
-        foreach ((new \ReflectionObject($e))->getProperties() as $sourceProperty) {
 
-            if (!in_array($sourceProperty->name, ['messages', 'severity', 'xdebug_message'])) {
+    if (PHP_VERSION_ID < 70001) {
 
-                $sourceProperty->setAccessible(true);
-                $destination->{$sourceProperty->getName()} = $sourceProperty->getValue($e);
+        /**
+         * Converts an \Exception into an array
+         *
+         * @param Exception $e
+         *
+         * @return string
+         */
+        function convert_exception_to_array(\Exception $e)
+        {
+            $destination        = new \stdClass();
+            $destination->class = get_class($e);
+            foreach ((new \ReflectionObject($e))->getProperties() as $sourceProperty) {
+
+                if (!in_array($sourceProperty->name, ['messages', 'severity', 'xdebug_message'])) {
+
+                    $sourceProperty->setAccessible(true);
+                    $destination->{$sourceProperty->getName()} = $sourceProperty->getValue($e);
+                }
             }
-        }
 
-        // return exception
-        return (array)$destination;
+            // return exception
+            return (array)$destination;
+        }
+    } else {
+
+        /**
+         * Converts an \Throwable into an array
+         *
+         * @param Throwable $e
+         *
+         * @return string
+         */
+        function convert_exception_to_array(\Throwable $e)
+        {
+            $destination        = new \stdClass();
+            $destination->class = get_class($e);
+            foreach ((new \ReflectionObject($e))->getProperties() as $sourceProperty) {
+
+                if (!in_array($sourceProperty->name, ['messages', 'severity', 'xdebug_message'])) {
+
+                    $sourceProperty->setAccessible(true);
+                    $destination->{$sourceProperty->getName()} = $sourceProperty->getValue($e);
+                }
+            }
+
+            // return exception
+            return (array)$destination;
+        }
     }
 }
